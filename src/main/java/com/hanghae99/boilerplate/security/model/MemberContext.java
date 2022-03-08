61f089e150e88d084a9658a9d95e02e076c74719
wns6743 <wns674@naver.com>
2022-03-08 19:24:26 +0900

    security에서만 사용

---
+++ src/main/java/com/hanghae99/boilerplate/security/model/MemberContext.java
@@ -0,0 +1,28 @@
+package com.hanghae99.boilerplate.security.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class MemberContext {
+
+
+    //principal로 catsing 되는 객체
+    //
+    private final String username;
+    private final List<GrantedAuthority> authorities;
+
+    public static MemberContext create(String email,List<GrantedAuthority> authorities){
+        if(email==null || email.isBlank()){
+            throw new IllegalArgumentException("email is Blank Or Null");
+        } else {
+            return new MemberContext(email,authorities);
+        }
+    }
+}
---
+++ src/main/java/com/hanghae99/boilerplate/security/service/UserDetailsImpl.java
@@ -0,0 +1,50 @@
+package com.hanghae99.boilerplate.security.service;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of="id")
+public class UserDetailsImpl implements UserDetails {
+
+    private Long id;
+    //email과 동일하다
+    private String username;
+    @JsonIgnore
+    private String password;
+
+    //유저가 가진 권한들
+    private Collection<? extends  GrantedAuthority> authorities;
+
+
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
