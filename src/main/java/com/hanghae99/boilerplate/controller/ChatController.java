321f27c95eba5518cdb55f7a3655fbcf727293a8
jeonghyeon <skjh0807s@snu.ac.kr>
2022-03-09 16:26:03 +0900

    webSocket으로 방개설, 채팅

---
+++ src/main/java/com/hanghae99/boilerplate/controller/ChatController.java
@@ -0,0 +1,26 @@
+package com.hanghae99.boilerplate.controller;
+
+import com.hanghae99.boilerplate.model.ChatRoom;
+import com.hanghae99.boilerplate.service.ChatService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/chat")
+public class ChatController {
+
+    private final ChatService chatService;
+
+    @PostMapping
+    public ChatRoom createRoom(@RequestParam String name) {
+        return chatService.createRoom(name);
+    }
+
+    @GetMapping
+    public List<ChatRoom> findAllRoom() {
+        return chatService.findAllRoom();
+    }
+}
