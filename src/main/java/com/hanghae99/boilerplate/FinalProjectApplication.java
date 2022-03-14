6d03956bdb69b4a69fdc1db3bbd1a0db5e222493
harimChoi <holly001503@gmail.com>
2022-03-14 21:04:04 +0900

    timejv

--- src/main/java/com/hanghae99/boilerplate/FinalProjectApplication.java
+++ src/main/java/com/hanghae99/boilerplate/FinalProjectApplication.java
@@ -2,6 +2,7 @@ package com.hanghae99.boilerplate;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.dom4j.rule.Mode;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
@@ -21,6 +22,9 @@ public class FinalProjectApplication {
 	@Bean
 	public ObjectMapper objectMapper(){
 		ObjectMapper objectMapper = new ObjectMapper();
+
+		objectMapper.registerModule(new JavaTimeModule());
+
 		objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,false);
 		return objectMapper;
 	}
