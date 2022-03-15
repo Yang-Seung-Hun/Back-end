10572ee4c31c3ae73460850e101b7643826f836c
wns6743 <wns674@naver.com>
2022-03-16 04:51:00 +0900

    테스트 커밋

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,22 +1,24 @@
 #!/usr/bin/env bash
 
-REPOSITORY=/opt/testapp
-cd $REPOSITORY
+REPOSITORY=/home/ubuntu/Mople
 
-APP_NAME=action_codedeploy
-JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
-JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
-CURRENT_PID=$(pgrep -f $APP_NAME)
+APP_NAME=boilerplate-0.0.1-SNAPSHOT.jar
 
+CURRENT_PID=$(pgrep -fl APP_NAME | grep java | awk '{print $1}')
 if [ -z $CURRENT_PID ]
 then
   echo "> 종료할것 없음."
 else
-  echo "> kill -9 $CURRENT_PID"
-  kill -15 $CURRENT_PID
+  echo "> kill -15 $CURRENT_PID"
+  sudo  kill -15 $CURRENT_PID
   sleep 5
 fi
 
-echo "> $JAR_PATH 배포"
-nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
+
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+
+
+chmod +x $JAR_NAME
+
+nohup java -jar  $JAR_NAME >  $REPOSITORY/nohup.out 2>&1 &
