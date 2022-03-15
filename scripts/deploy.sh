cfeee7e28fd5f48db4d71538b2d0a64fe86d51bd
최호준 <67067346+wns6743@users.noreply.github.com>
2022-03-16 05:12:17 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,17 +8,22 @@ APP_NAME=boilerplate-0.0.1-SNAPSHOT.jar
 CURRENT_PID=$(pgrep -fl APP_NAME | grep java | awk '{print $1}')
 if [ -z $CURRENT_PID ]
 then
-  echo "> 종료할것 없음."
+  echo "> 종료할것 없음." 
+  
 else
   echo "> kill -15 $CURRENT_PID"
   sudo  kill -15 $CURRENT_PID
   sleep 5
 fi
 
+echo "> 새 어플리케이션 배포"
 
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
-
+echo "> JAR Name: $JAR_NAME"
+echo "> $JAR_NAME에 실행권한 추가"
 chmod +x $JAR_NAME
 
+echo "> 실행 "
 nohup java -jar  $JAR_NAME >  $REPOSITORY/nohup.out 2>&1 &
+echo "> ========================================================"
