79e6f714f02515618a49f961007232a992b22858
wns6743 <wns674@naver.com>
2022-03-15 16:14:34 +0900

    테스트 커밋

---
+++ src/main/java/com/hanghae99/boilerplate/Test.java
@@ -0,0 +1,13 @@
+package com.hanghae99.boilerplate;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Test {
+
+    @GetMapping("/")
+    public String test(){
+        return "hello";
+    }
+}
