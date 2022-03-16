871a11e376ae4267e617e62ac5552d36e3a1673d
최호준 <67067346+wns6743@users.noreply.github.com>
2022-03-16 17:21:06 +0900

    자동실행 에러 해결

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,8 +7,7 @@ APP_NAME=boilerplate
 CURRENT_PID=$(pgrep -fl $APP_NAME | grep java | awk '{print $1}')
 if [ -z $CURRENT_PID ]
 then
-  echo "> 종료할것 없음." 
-
+  echo "> 종료할것 없음."
 
 else
   echo "> kill -15 $CURRENT_PID"
@@ -16,14 +15,14 @@ else
   sleep 5
 fi
 
-echo "> 새 어플리케이션 배포"
 
-JAR_NAME=boilerplate-0.0.1-SNAPSHOT.jar
+rm  -rf $REPOSITORY/boilerplate-0.0.1-SNAPSHOT-plain.jar
 
+
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 echo "> JAR Name: $JAR_NAME"
-echo "> $JAR_NAME에 실행권한 추가"
 chmod +x $JAR_NAME
 
 echo "> 실행 "
-nohup java -jar  $JAR_NAME >  $REPOSITORY/nohup.out 2>&1 &
+sudo nohup java -jar  $JAR_NAME >  $REPOSITORY/nohup.out 2>&1 &
 echo "> ========================================================"
