b9de7c0e8dab5572e5b3be89abdd4603cc7596b5
wns6743 <wns674@naver.com>
2022-03-22 06:26:55 +0900

    EB TEST

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,28 +1,28 @@
-#!/usr/bin/env bash
-
-REPOSITORY=/home/ubuntu/Mople
-
-APP_NAME=boilerplate
-
-CURRENT_PID=$(pgrep -fl $APP_NAME | grep java | awk '{print $1}')
-if [ -z $CURRENT_PID ]
-then
-  echo "> 종료할것 없음."
-
-else
-  echo "> kill -15 $CURRENT_PID"
-  sudo  kill -15 $CURRENT_PID
-  sleep 5
-fi
-
-
-rm  -rf $REPOSITORY/boilerplate-0.0.1-SNAPSHOT-plain.jar
-
-
-JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
-echo "> JAR Name: $JAR_NAME"
-chmod +x $JAR_NAME
-
-echo "> 실행 "
-sudo nohup java -jar  $JAR_NAME >  $REPOSITORY/nohup.out 2>&1 &
-echo "> ========================================================"
+##!/usr/bin/env bash
+#
+#REPOSITORY=/home/ubuntu/Mople
+#
+#APP_NAME=boilerplate
+#
+#CURRENT_PID=$(pgrep -fl $APP_NAME | grep java | awk '{print $1}')
+#if [ -z $CURRENT_PID ]
+#then
+#  echo "> 종료할것 없음."
+#
+#else
+#  echo "> kill -15 $CURRENT_PID"
+#  sudo  kill -15 $CURRENT_PID
+#  sleep 5
+#fi
+#
+#
+#rm  -rf $REPOSITORY/boilerplate-0.0.1-SNAPSHOT-plain.jar
+#
+#
+#JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+#echo "> JAR Name: $JAR_NAME"
+#chmod +x $JAR_NAME
+#
+#echo "> 실행 "
+#sudo nohup java -jar  $JAR_NAME >  $REPOSITORY/nohup.out 2>&1 &
+#echo "> ========================================================"
