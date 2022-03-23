f933806ad6e3678270e620ef7427c4489bfc2eb5
wns6743 <wns674@naver.com>
2022-03-23 18:33:42 +0900

    회원가입테스트용추가

---
+++ src/main/java/com/hanghae99/boilerplate/signupLogin/test/Service.java
@@ -0,0 +1,33 @@
+package com.hanghae99.boilerplate.signupLogin.test;
+
+import com.hanghae99.boilerplate.memberManager.model.Member;
+import com.hanghae99.boilerplate.memberManager.repository.MemberRepository;
+import com.hanghae99.boilerplate.security.config.RefreshTokenRedis;
+import com.hanghae99.boilerplate.security.model.MemberContext;
+import com.hanghae99.boilerplate.signupLogin.dto.requestDto.SignupReqestDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+
+@org.springframework.stereotype.Service
+public class Service {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+    @Autowired
+    RefreshTokenRedis redis;
+
+    @Transactional
+    public MemberContext signupRequest(SignupReqestDto signupReqestDto) {
+        boolean result = memberRepository.existsMemberByEmail(signupReqestDto.getEmail());
+        if (result) {
+            throw new IllegalArgumentException(signupReqestDto.getEmail() + " already" + "Exist!");
+        }
+        signupReqestDto.setPassword(passwordEncoder.encode(signupReqestDto.getPassword()));
+        Member member= memberRepository.save(new Member(signupReqestDto));
+        return new MemberContext(member);
+    }
+}
