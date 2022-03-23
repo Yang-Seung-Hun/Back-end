3bbc5d1440a11435c88be8e45f16efcb4c8dbea6
wns6743 <wns674@naver.com>
2022-03-23 21:26:09 +0900

    EB-TEST

--- src/main/java/com/hanghae99/boilerplate/memberManager/mail/controller/MailController.java
+++ src/main/java/com/hanghae99/boilerplate/memberManager/mail/controller/MailController.java
@@ -25,7 +25,7 @@ public class MailController {
     @GetMapping("/")
     public  void healthCheck(HttpServletResponse response) throws IOException {
         response.setStatus(HttpStatus.OK.value());
-        objectMapper.writeValue(response.getWriter(),ResponseDto.of(HttpStatus.OK,"그만그만ㄱ므ㅏㄴ",null));
+        objectMapper.writeValue(response.getWriter(),ResponseDto.of(HttpStatus.OK,"제발...*",null));
     }
 
     @PostMapping("/api/user/mypw")
